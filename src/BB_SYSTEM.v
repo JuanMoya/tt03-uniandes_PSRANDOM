d02803d1b7fe2507ae3aff59bec1927fb1dc9203
JuanMoya <jsmoya07@gmail.com>
2023-03-23 13:41:00 -0500

    Update BB_SYSTEM.v

--- src/BB_SYSTEM.v
+++ src/BB_SYSTEM.v
@@ -1,7 +1,7 @@
 /*######################################################################
-//#	G0B1T: HDL EXAMPLES. 2018.
+//#	G0B1T: HDL EXAMPLES. 2023.
 //######################################################################
-//# Copyright (C) 2018. F.E.Segura-Quijano (FES) fsegura@uniandes.edu.co
+//# Copyright (C) 2023. F.E.Segura-Quijano (FES) fsegura@uniandes.edu.co
 //# 
 //# This program is free software: you can redistribute it and/or modify
 //# it under the terms of the GNU General Public License as published by
@@ -18,15 +18,13 @@
 //=======================================================
 //  MODULE Definition
 //=======================================================
+`default_nettype none
+
 module BB_SYSTEM (
-//////////// OUTPUTS //////////
-	BB_SYSTEM_data_OutBUS,
-//////////// INPUTS //////////
-	BB_SYSTEM_CLOCK_50,
-	BB_SYSTEM_RESET_InHigh,
-	BB_SYSTEM_clear_InLow, 
-	BB_SYSTEM_load_InLow
+  input [7:0] io_in,
+  output [7:0] io_out
 );
+
 //=======================================================
 //  PARAMETER declarations
 //=======================================================
@@ -34,31 +32,27 @@ parameter DATAWIDTH_BUS = 8;
 //=======================================================
 //  PORT declarations
 //=======================================================
-output		[DATAWIDTH_BUS-1:0]	BB_SYSTEM_data_OutBUS;
-input		BB_SYSTEM_CLOCK_50;
-input		BB_SYSTEM_RESET_InHigh;
-input		BB_SYSTEM_clear_InLow;
-input		BB_SYSTEM_load_InLow;
+wire		[DATAWIDTH_BUS-1:0]	BB_SYSTEM_data_OutBUS;
+assign 		io_out[7:0] = BB_SYSTEM_data_OutBUS;
+
+wire		BB_SYSTEM_CLOCK_50 = io_in[0];
+wire		BB_SYSTEM_RESET_InHigh = io_in[1];
+wire		BB_SYSTEM_load_InLow = io_in[2];
+wire		BB_SYSTEM_rand_InLow = io_in[3];
+wire		[DATAWIDTH_BUS-5:0] BB_SYSTEM_data_InBUS;
+assign		BB_SYSTEM_data_InBUS = io_in[3:0];
 //=======================================================
 //  REG/WIRE declarations
 //=======================================================
-wire	STATEMACHINE_clear_cwire;
 wire	STATEMACHINE_load_cwire;
-wire 	BB_SYSTEM_clear_InLow_cwire;
+wire	STATEMACHINE_rand_cwire;
 wire 	BB_SYSTEM_load_InLow_cwire;
+wire 	BB_SYSTEM_rand_InLow_cwire;
 wire 	[DATAWIDTH_BUS-1:0] PSRANDOM_to_RegGENERAL_cwire;
 //=======================================================
 //  Structural coding
 //=======================================================
 SC_DEBOUNCE1 SC_DEBOUNCE1_u0 (
-// port map - connection between master ports and signals/registers   
-	.SC_DEBOUNCE1_button_Out(BB_SYSTEM_clear_InLow_cwire),
-	.SC_DEBOUNCE1_CLOCK_50(BB_SYSTEM_CLOCK_50),
-	.SC_DEBOUNCE1_RESET_InHigh(BB_SYSTEM_RESET_InHigh),
-	.SC_DEBOUNCE1_button_In(BB_SYSTEM_clear_InLow)
-);
-
-SC_DEBOUNCE1 SC_DEBOUNCE1_u1 (
 // port map - connection between master ports and signals/registers   
 	.SC_DEBOUNCE1_button_Out(BB_SYSTEM_load_InLow_cwire),
 	.SC_DEBOUNCE1_CLOCK_50(BB_SYSTEM_CLOCK_50),
@@ -66,21 +60,30 @@ SC_DEBOUNCE1 SC_DEBOUNCE1_u1 (
 	.SC_DEBOUNCE1_button_In(BB_SYSTEM_load_InLow)
 );
 
+SC_DEBOUNCE1 SC_DEBOUNCE1_u1 (
+// port map - connection between master ports and signals/registers   
+	.SC_DEBOUNCE1_button_Out(BB_SYSTEM_rand_InLow_cwire),
+	.SC_DEBOUNCE1_CLOCK_50(BB_SYSTEM_CLOCK_50),
+	.SC_DEBOUNCE1_RESET_InHigh(BB_SYSTEM_RESET_InHigh),
+	.SC_DEBOUNCE1_button_In(BB_SYSTEM_rand_InLow)
+);
+
 	SC_STATEMACHINE SC_STATEMACHINE_u0 (
 // port map - connection between master ports and signals/registers   
-	.SC_STATEMACHINE_clear_OutLow(STATEMACHINE_clear_cwire), 
 	.SC_STATEMACHINE_load_OutLow(STATEMACHINE_load_cwire), 
+	.SC_STATEMACHINE_rand_OutLow(STATEMACHINE_rand_cwire), 
 	.SC_STATEMACHINE_CLOCK_50(BB_SYSTEM_CLOCK_50),
 	.SC_STATEMACHINE_RESET_InHigh(BB_SYSTEM_RESET_InHigh),
-	.SC_STATEMACHINE_clear_InLow(BB_SYSTEM_clear_InLow_cwire), 
-	.SC_STATEMACHINE_load_InLow(BB_SYSTEM_load_InLow_cwire)
+	.SC_STATEMACHINE_load_InLow(BB_SYSTEM_load_InLow_cwire),
+	.SC_STATEMACHINE_rand_InLow(BB_SYSTEM_rand_InLow_cwire)
 );
 
 SC_PSRANDOM #(.RegGENERAL_DATAWIDTH(DATAWIDTH_BUS)) SC_PSRANDOM_u0 (
 // port map - connection between master ports and signals/registers   
 	.SC_PSRANDOM_data_OutBUS(PSRANDOM_to_RegGENERAL_cwire),
 	.SC_PSRANDOM_CLOCK_50(BB_SYSTEM_CLOCK_50),
-	.SC_PSRANDOM_RESET_InHigh(BB_SYSTEM_RESET_InHigh)
+	.SC_PSRANDOM_RESET_InHigh(BB_SYSTEM_RESET_InHigh),
+	.SC_PSRANDOM_data_InBUS(BB_SYSTEM_data_InBUS)
 );
 
 SC_RegGENERAL #(.RegGENERAL_DATAWIDTH(DATAWIDTH_BUS)) SC_RegGENERAL_u0 (
@@ -88,8 +91,8 @@ SC_RegGENERAL #(.RegGENERAL_DATAWIDTH(DATAWIDTH_BUS)) SC_RegGENERAL_u0 (
 	.SC_RegGENERAL_data_OutBUS(BB_SYSTEM_data_OutBUS),
 	.SC_RegGENERAL_CLOCK_50(BB_SYSTEM_CLOCK_50),
 	.SC_RegGENERAL_RESET_InHigh(BB_SYSTEM_RESET_InHigh),
-	.SC_RegGENERAL_clear_InLow(STATEMACHINE_clear_cwire),
 	.SC_RegGENERAL_load_InLow(STATEMACHINE_load_cwire),
+	.SC_RegGENERAL_rand_InLow(STATEMACHINE_rand_cwire),
 	.SC_RegGENERAL_data_InBUS(PSRANDOM_to_RegGENERAL_cwire)
 );
 
